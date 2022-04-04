ab13a960403eb4f2a37040102f992306b9aa9bc2
Tigermouthbear <tigermouthbear@tigr.dev>
2022-04-03 23:27:45 -0400

    improve statistics performance

--- stats/src/main/kotlin/dev/tigr/ttvattendance/stats/Main.kt
+++ stats/src/main/kotlin/dev/tigr/ttvattendance/stats/Main.kt
@@ -8,9 +8,6 @@ import org.jetbrains.exposed.sql.transactions.transaction
 // logs into sql server and generates stats for ttvattendance
 // setup for postgres, but is easily changed. see jetbrains exposed documentation
 fun main(args: Array<String>) {
-    // set logger to only warnings
-    System.setProperty("org.slf4j.simplelogger.defaultLogLevel", "WARNING")
-
     if(args.size < 4 || args.size > 5) {
         println("Missing the correct arguments!\nUsage: java -jar stats.jar [streamer] [url] [user] [password] [range (optional)]")
         return
@@ -18,7 +15,6 @@ fun main(args: Array<String>) {
 
     // data we are collecting
     var streamsLength = 0 // number of streams
-    var streams: ArrayList<Int>? = null // ids of streams in range, null if no range
     var total = 0 // total number of viewers
     var active = 0 // total number of viewers who have shown up to half of the streams
     var dedicated = 0 // total number of viewers who have came to each of the streams
@@ -32,14 +28,12 @@ fun main(args: Array<String>) {
 
     // get total streams in range
     val ranged = args.size == 5 && args[4].contains("-")
-    if(ranged) streams = arrayListOf()
     val min: Int = if(ranged) args[4].split("-")[0].toInt() else 0
     val max: Int = if(ranged) args[4].split("-")[1].toInt() else Int.MAX_VALUE
     transaction(database) {
         streamTable.selectAll().forEach {
             val id = it[streamTable.date]
             if(id in min..max) {
-                if(ranged) streams?.add(id)
                 streamsLength++
             }
         }
@@ -47,7 +41,7 @@ fun main(args: Array<String>) {
 
     // get total number of viewers
     transaction(database) {
-        chartTable.selectAllBatched().forEach { batch -> batch.forEach {
+        chartTable.selectAllBatched(batchSize = 10_000).forEach { batch -> batch.forEach {
             if(!ranged) {
                 // check all
                 val num = it[chartTable.streams].size
@@ -58,7 +52,7 @@ fun main(args: Array<String>) {
                 // check ranged
                 val attended = it[chartTable.streams]
                 var num = 0
-                attended.forEach { stream -> if(streams!!.contains(stream)) num++ }
+                attended.forEach { stream -> if(stream in min..max) num++ }
                 if(num == streamsLength) dedicated++
                 else if(num > streamsLength / 2) active++
                 if(num > 0) total++
